470c260765f110ef70218f0f5d17c890c06425ab
kristin helgadottir <stina-stud@live.dk>
2017-08-02 09:20:48 +0200

    Update Flights.java
    Removing unnessisary text.

--- src/main/java/entities/Flights.java
+++ src/main/java/entities/Flights.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entities;
 
 import java.io.Serializable;
@@ -13,10 +8,6 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-/**
- *
- * @author jarmo
- */
 @Entity
 @Table(name = "flights")
 @NamedQueries({
